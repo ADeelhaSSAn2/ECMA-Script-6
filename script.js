3a663868217f81fb0b0cbf1e00a259d2f73cac7d
Adeel Hassan <adeelhassan35033@gmail.com>
2024-04-29 22:20:04 +0500

    update

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 // console.log(str)
 // var str = "HI";
 
-let str ="adeel";
-str ="hassan";
-console.log(str)
+// let str ="adeel";
+// str ="hassan";
+// console.log(str)
 
 
 // const tr ="adeel";
@@ -20,17 +20,17 @@ console.log(str)
 
 
 // global scope hai
-if(true){
-    var a = "test";
-}
-console.log(a)
+// if(true){
+//     var a = "test";
+// }
+// console.log(a)
 
-// function scope hai
-function abc(){
-    var a = "test";
-}
-abc()
-console.log(a)
+// // function scope hai
+// function abc(){
+//     var a = "test";
+// }
+// abc()
+// console.log(a)
 
 
 
@@ -50,6 +50,84 @@ console.log(a)
 
 // template string
 
-let firstName ="Adeel";
-let lastName = "Hassan";
-console.log(`${firstName}${lastName}`)
+// let firstName ="Adeel";
+// let lastName = "Hassan";
+// console.log(`${firstName}${lastName}`)
+
+
+// Desturctring;
+
+// let student = {
+//     name : "Adeel",
+//     roll : 232323,
+//     email : "adeel@123",
+//     school:{
+//         title: "smit",
+//     },
+// }
+//  const {name, email , roll} = student;
+
+//  console.log(student.name)
+// console.log(student.email)
+
+
+// let arr = ["aa" , "bb" , "cc"];
+
+// const[a , c , b] = arr;
+// console.log(a, b , c);
+
+
+
+// default + rest + spread operators
+
+
+// default 
+
+// function foo(a , b =2){
+// return a + b
+// }
+// console.log(foo(5,5));
+
+
+// rest operators
+
+// function adel(c,d,e,f,i){
+//     console.log(c,d,e,f,i)
+// }
+// function adel(...rest){
+//     console.log(rest)
+// }
+// adel("Adeel","Zohaib","Fahad","Saad","Minhaj")
+
+// // spread operators
+
+let obj = {
+    name: "adeel",
+    email: "adeel@321",
+}
+
+let obj1 = {
+    school: "smit",
+    class: "wam",
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// quizz app
+
+
